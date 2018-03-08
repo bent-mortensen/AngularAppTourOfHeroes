644c6ebf470d93fd116d7b4a9ca6e87bb537815f
bent_ <bent_@DESKTOP-GU5KU5K>
2018-03-08 15:28:25 +0100

    tourofheroes to github

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Spiderman' },
+    { id: 12, name: 'Superman' },
+    { id: 13, name: 'Batman' },
+    { id: 14, name: 'Darth Vader' },
+    { id: 15, name: 'Antman' },
+    { id: 16, name: 'Wolverine' },
+    { id: 17, name: 'The Hulk' },
+    { id: 18, name: 'Spawn' },
+    { id: 19, name: 'Blade' },
+    { id: 20, name: 'Tintin' }
+];
